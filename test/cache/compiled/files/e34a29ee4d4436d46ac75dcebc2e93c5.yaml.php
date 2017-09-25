4fe91ba97bd0edddbcefaab856125db386f4c04e
Brooke Hopkins <brooke.hopkins@nyu.edu>
2017-09-25 05:30:00 +0400

    Update page

--- test/cache/compiled/files/e34a29ee4d4436d46ac75dcebc2e93c5.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/Users/brookehopkins/Downloads/grav-skeleton-woo-site/user/config/security.yaml',
-    'modified' => 1504378900,
-    'data' => [
-        'salt' => 'AzbEM0f6xRPnYX'
-    ]
-];
